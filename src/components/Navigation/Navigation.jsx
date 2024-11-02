2631d6f5a4eb8ac45b6a19aa82ba0bafe438489f
matthewjung04 <matthew.jung95@gmail.com>
2024-11-02 00:46:41 -0400

    navigation bar complete

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,18 +4,83 @@ import dropDownArrow from '../../assets/icons/arrow_drop_down.svg'
 import './Navigation.scss'
 
 function Navigation() {
+
+  const hoverEffect = (e) => {
+    const menuID = e.target.id;
+
+    if(menuID == 'consumer-seller-menu') {
+      document.getElementById('consumer-seller-dropdown').style.display = 'block';
+      document.getElementById('product-inspector-dropdown').style.display = 'none';
+
+    } else if(menuID == 'product-inspector-menu') {
+      document.getElementById('consumer-seller-dropdown').style.display = 'none';
+      document.getElementById('product-inspector-dropdown').style.display = 'block';
+    }
+  }
+
+  const hoverExit = () => {
+    document.getElementById('consumer-seller-dropdown').style.display = 'none';
+    document.getElementById('product-inspector-dropdown').style.display = 'none';
+  }
+
   return (
     <section className='header'>
       <Link to='/'><img src={logo} alt="logo-image"/></Link>
       <h1 className='header__title'>Amazon Product Review Analysis</h1>
-      <div className='header__nav'>
-        <Link to='/consumer-seller' className='header__nav__item'>
-          Consumers & Sellers<img src={dropDownArrow} alt='drop_arrow_icon'/>
-        </Link>
-        <Link to='/product-inspector' className='header__nav__item'>
-          Product Inspector<img src={dropDownArrow} alt='drop_arrow_icon'/>
-        </Link>
-      </div>
+      <article className='header__nav'>
+        <div className='header__nav__menu'>
+          <Link 
+            to='/consumer-seller' 
+            className='header__nav__menu__item'
+            id='consumer-seller-menu' 
+            onMouseOver={hoverEffect}>
+            Consumers & Sellers<img src={dropDownArrow} alt='drop_arrow_icon'/>
+          </Link>
+          <div className='header__nav__menu__item__dropDown' id='consumer-seller-dropdown' onMouseLeave={hoverExit}>
+            <Link
+              to='/consumer-seller/tableau-dashboard'
+              className='header__nav__menu__item__dropDown__items'>
+              Tableau Dashboard
+            </Link>
+            <Link
+              to='/consumer-seller/pros-and-cons'
+              className='header__nav__menu__item__dropDown__items'>
+              Pros and Cons
+            </Link>
+            <Link
+              to='/consumer-seller/top-reviews'
+              className='header__nav__menu__item__dropDown__items'>
+              Top Reviews
+            </Link>
+          </div>
+        </div>
+        <div className='header__nav__menu'> 
+          <Link 
+            to='/product-inspector'
+            className='header__nav__menu__item'
+            id='product-inspector-menu'
+            onMouseOver={hoverEffect}>
+            Product Inspector<img src={dropDownArrow} alt='drop_arrow_icon'/>
+          </Link>
+          <div className='header__nav__menu__item__dropDown' id='product-inspector-dropdown' onMouseLeave={hoverExit}>
+            <Link 
+              to='/product-inspector/tableau-dashboard'
+              className='header__nav__menu__item__dropDown__items'>
+              Tableau Dashboard
+            </Link>
+            <Link
+              to='/product-inspector/pros-and-cons'
+              className='header__nav__menu__item__dropDown__items'>
+              Pros and Cons
+            </Link>
+            <Link
+              to='/product-inspector/top-reviews'
+              className='header__nav__menu__item__dropDown__items'>
+              Top Reviews
+            </Link>
+          </div>
+        </div>
+      </article>
     </section>
   )
 }
